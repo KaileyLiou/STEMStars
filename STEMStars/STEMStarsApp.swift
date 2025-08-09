a615beb6a454757daf7b448922d60ed8f9ac6643
KaileyLiou <kaileyliou09@gmail.com>
2025-08-09 18:02:12 -0400

    Initial Commit

---
+++ STEMStars/STEMStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMStarsApp.swift
+//  STEMStars
+//
+//  Created by Scholar on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct STEMStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
